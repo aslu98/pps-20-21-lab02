afb55c7b95739b586fb603d516478017be6702b9
Asia <asia.lucchi@studio.unibo.it>
2021-03-08 14:17:03 +0100

    ex 2 completed

---
+++ src/u02/ex3/Fibonacci.scala
@@ -0,0 +1,23 @@
+package u02.ex3
+
+import org.junit.jupiter.api.Assertions._
+import org.junit.jupiter.api.Test
+
+object Fibonacci{
+  def fib (n: Int) : Int = n match {
+   case 0 => 0
+   case 1 => 1
+   case _ => fib (n - 1) + fib(n - 2)
+  }
+  /*non è tail perchè dopo aver eseguito la chiamata alla funzione ricorsiva non basta ritornarla, bisogna sommarla con l'altra chiamata*/
+
+  @Test def testFib(): Unit ={
+    assertEquals(0, fib(0));
+    assertEquals(1, fib(1));
+    assertEquals(1, fib(2));
+    assertEquals(2, fib(3));
+    assertEquals(3, fib(4));
+    assertEquals(5, fib(5));
+    assertEquals(8, fib(6));
+  }
+}
