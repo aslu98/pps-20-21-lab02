0f9ff8f0f9d2dcf4d439b9df03e84263e82f4930
Asia <asia.lucchi@studio.unibo.it>
2021-03-09 11:53:11 +0100

    challenge (Generalistion+DRY+Strategy) implemented

--- src/u02/part5/Optionals.scala
+++ src/u02/part5/Optionals.scala
@@ -28,35 +28,29 @@ object Optionals {
 
     def filter[A](opt: Option[A])(f: A => Boolean) : Option[A] = opt match {
       case None() => None()
-      case Some(a: A) => {
-        f(a) match {
+      case Some(a: A) => f(a) match {
           case true => Some(a)
           case _ => None()
         }
-      }
     }
 
     def map[A](opt: Option[A])(f: A => Boolean) : Option[Boolean] = opt match {
       case None() => None()
-      case Some(a:A) => {
-        f(a) match {
+      case Some(a:A) => f(a) match {
           case true => Some(true)
           case _ => None()
         }
-      }
     }
 
     def map2[A](opt1: Option[A], opt2: Option[A]) : Option[A] = (opt1, opt2) match{
       case (Some(a: A), Some(b:A)) => Two(a, b)
       case _ => None()
     }
+
+    def modify[A](opt: Option[A], f: Option[A] => Option[A]) : Option[A] = f(opt)
   }
 
   import Option._
-
-  val s1: Option[Int] = Some(1)
-  val s2: Option[Int] = Some(2)
-  val s3: Option[Int] = None()
 
   @Test def testMap(): Unit ={
     assertEquals(None(), map(None[Int])(_ > 2)) // None
@@ -64,12 +58,32 @@ object Optionals {
   }
 
   @Test def testMap2(): Unit ={
-    assertEquals(None(), map2(None[Int], Some(1))) // None
+    assertEquals(None(), map2(None(), None())) // None
+    assertEquals(None(), map2(None(), Some(1))) // None
     assertEquals(Two(5,3), map2(Some(5), Some(3)))
   }
 
   @Test def testFilter(): Unit ={
-    assertEquals(Some(5), filter(Some(5))(_>2)) // None
+    assertEquals(Some(5), filter(Some(5))(_>2))
     assertEquals(None(), filter(Some(5))(_>8))
+  }
+
+  @Test def testDouble(): Unit ={
+    val double: Option[Int] => Option[Int] = {
+      case Some(a) => Some(a * 2)
+      case _ => None()
+    }
+    assertEquals(Some(10), modify(Some(5), double))
+    assertEquals(None(), modify(None(), double))
+  }
+
+  @Test def tesNeg(): Unit ={
+    val neg: Option[Boolean] => Option[Boolean] = {
+      case Some(a) => Some(!a)
+      case _ => None()
+    }
+
+    assertEquals(Some(false), modify(Some(true), neg))
+    assertEquals(None(), modify(None(), neg))
   }
 }
